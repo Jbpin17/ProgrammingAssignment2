a47c03d51d8a38d0718474f8192dc935235a57ce
Jbpin17 <jbpin17@gmail.com>
2025-02-26 14:09:08 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,64 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#R Programming Assignment 2
+#Make Vector Function
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
+#Cache Mean Function
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+#Creating A Matrix for the Cache
+makeCacheMat <- function(x=matrix(sample(1:125,16),4,4)){
+  z <- NULL
+  set <- function(y){
+    x <-y
+    z <- NULL
+  }
+  get <- function()x
+  setsolve <- function(solve) z <<- solve
+  getsolve <- function() z
+  list(set=set, get = get, setsolve=setsolve, getsolve=getsolve)
+}
+
+#Cache Solving Function
+cacheSolve <- function(x,...){
+  z <- x$getsolve()
+  if(!is.null(z)){
+    message("inverse matrix")
+    return(z)
+  }
+## Return a matrix that is the inverse of 'x'
+  data <- x$get()
+  z<- solve(data,...)
+  x$setsolve(z)
+  z
+}
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
 }
