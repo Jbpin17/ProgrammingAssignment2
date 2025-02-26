6b2b5e23032b07988dd560ac99036a1251844c61
Jbpin17 <jbpin17@gmail.com>
2025-02-26 14:59:14 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -33,6 +33,7 @@ cachemean <- function(x, ...) {
 }
 
 #Creating A Matrix for the Cache
+#makeCacheMat is a function which creates a special "matrix" object that can cache its inverse for the input (which is an invertible square matrix)
 makeCacheMat <- function(x=matrix(sample(1:125,16),4,4)){
   z <- NULL
   set <- function(y){
@@ -46,6 +47,7 @@ makeCacheMat <- function(x=matrix(sample(1:125,16),4,4)){
 }
 
 #Cache Solving Function
+#cacheSolve is a function which computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache
 cacheSolve <- function(x,...){
   z <- x$getsolve()
   if(!is.null(z)){
@@ -59,8 +61,3 @@ cacheSolve <- function(x,...){
   z
 }
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        
-}
